70836e4485178237b07f1eaa197a4e46bb86c232
Justin <justinwhitaker2016@gmail.com>
2018-04-10 18:05:13 -0700

    got all random cookie generator functions working for 15 time points and inserted into cookieEachHour array

--- app.js
+++ app.js
@@ -6,17 +6,28 @@ var cookieStorePike = {
   locationurl: 'https://www.google.com/maps/place/1st+Ave+%26+Pike+St,+Seattle,+WA+98101/@47.6088188,-122.3422157,17z/data=!3m1!4b1!4m5!3m4!1s0x54906ab2f8f1e7f5:0xbb338c128f949a19!8m2!3d47.6088152!4d-122.340027',
   minCust: 23,
   maxCust: 65,
+  storeOpen: 6,
+  storeClose: 21,
   aveCookieSale: 6.3,
-  custEachHour: [],
-  randCust: function () {
+  //array for storing hourly cookie sales
+  cookieEachHour: [],
+  //function for generating random number of customers
+  randCust: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  },
+  //function for taking random # of customers, mutiplying them by the number of cookies
+  //bought each hour, and then pushing the resulting information into the custEachHour array
+  cookieGen: function() {
+    for (var i = (this.storeClose - this.storeOpen); i > 0; i --) {
+      var cookieHour = this.randCust()*this.aveCookieSale;
+      this.cookieEachHour.push(Math.round(cookieHour));
+    }
   }
 };
-for (var i = 15; i > 0; i --) {
-  var hour = cookieStorePike.randCust();
-  cookieStorePike.custEachHour.push(hour);
-}
-console.log(cookieStorePike.custEachHour);
+cookieStorePike.cookieGen();
+console.log('The hourly cookie sales at the First and Pike location are:',cookieStorePike.cookieEachHour);
+
+
 
 
 var cookieStoreSeatac = {
@@ -24,18 +35,23 @@ var cookieStoreSeatac = {
   locationurl: '',
   minCust: 3,
   maxCust: 24,
+  storeOpen: 6,
+  storeClose: 21,
   aveCookieSale: 1.2,
-  custEachHour: [],
+  cookieEachHour: [],
   randCust: function () {
     return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  },
+  cookieGen: function() {
+    for (var i = (this.storeClose - this.storeOpen); i > 0; i --) {
+      var cookieHour = this.randCust()*this.aveCookieSale;
+      this.cookieEachHour.push(Math.round(cookieHour));
+    }
   }
 };
-for (var i = 15; i > 0; i --) {
-  var hour = cookieStoreSeatac.randCust();
-  cookieStoreSeatac.custEachHour.push(hour);
-}
 
-console.log(cookieStoreSeatac.custEachHour);
+cookieStoreSeatac.cookieGen();
+console.log('The hourly cookie sales at the SeaTac location are:',cookieStoreSeatac.cookieEachHour);
 
 
 var cookieStoreSeaCent =
@@ -44,18 +60,23 @@ var cookieStoreSeaCent =
   locationurl: '',
   minCust: 11,
   maxCust: 38,
+  storeOpen: 6,
+  storeClose: 21,
   aveCookieSale: 6.3,
-  custEachHour: [],
+  cookieEachHour: [],
   randCust: function ()
   {
     return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  },
+  cookieGen: function() {
+    for (var i = (this.storeClose - this.storeOpen); i > 0; i --) {
+      var cookieHour = this.randCust()*this.aveCookieSale;
+      this.cookieEachHour.push(Math.round(cookieHour));
+    }
   }
 };
-for (var i = 15; i > 0; i --) {
-  var hour = cookieStoreSeaCent.randCust();
-  cookieStoreSeaCent.custEachHour.push(hour);
-}
-console.log(cookieStoreSeaCent.custEachHour);
+cookieStoreSeaCent.cookieGen();
+console.log('The hourly cookie sales for the Seattle Center location are:',cookieStoreSeaCent.cookieEachHour);
 
 
 var cookieStoreCapHill = {
@@ -63,19 +84,23 @@ var cookieStoreCapHill = {
   locationurl: '',
   minCust: 20,
   maxCust: 38,
+  storeOpen: 6,
+  storeClose: 21,
   aveCookieSale: 2.3,
-  custEachHour: [],
-  randCust: function ()
-  {
+  cookieEachHour: [],
+  randCust: function () {
     return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
   },
+  cookieGen: function() {
+    for (var i = (this.storeClose - this.storeOpen); i > 0; i --) {
+      var cookieHour = this.randCust()*this.aveCookieSale;
+      this.cookieEachHour.push(Math.round(cookieHour));
+    }
+  }
 };
-for (var i = 15; i > 0; i --) {
-  var hour = cookieStoreCapHill.randCust();
-  cookieStoreCapHill.custEachHour.push(hour);
-}
-console.log(cookieStoreCapHill.custEachHour);
 
+cookieStoreCapHill.cookieGen();
+console.log('The hourly cookie sales for the Capitol Hill location are:',cookieStoreCapHill.cookieEachHour);
 
 var cookieStoreAlki =
 {
@@ -83,16 +108,21 @@ var cookieStoreAlki =
   locationurl: '',
   minCust: 2,
   maxCust: 16,
+  storeOpen: 6,
+  storeClose: 21,
   aveCookieSale: 4.6,
-  custEachHour: [],
-  randCust: function ()
-  {
+  cookieEachHour: [],
+  randCust: function () {
     return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  },
+  cookieGen: function() {
+    for (var i = (this.storeClose - this.storeOpen); i > 0; i --) {
+      var cookieHour = this.randCust()*this.aveCookieSale;
+      this.cookieEachHour.push(Math.round(cookieHour));
+    }
   }
 };
+cookieStoreAlki.cookieGen();
+console.log('The hourly cookie sales for the Alki location are:', cookieStoreAlki.cookieEachHour);
 
-for (var i = 15; i > 0; i --) {
-  var hour = cookieStoreAlki.randCust();
-  cookieStoreAlki.custEachHour.push(hour);
-}
-console.log(cookieStoreAlki.custEachHour);
+// stretch goal: putting each company's hours up on their website
